82d7ed8d8ef2b4f53615583f06e01937fbd14a23
Parth Vasant Kataria <kataria.p@northeastern.edu>
2022-10-20 03:17:35 -0400

    cicd

--- packer/app.sh
+++ packer/app.sh
@@ -10,7 +10,6 @@ sudo bash nodesource_setup.sh
 sudo apt-get install nodejs -y
 sudo apt-get install npm -y
 sudo apt install unzip
-cd ~/ && unzip webapp.zip
 cd ~/webapp && npm i
 
 echo "Installing mysql server"
