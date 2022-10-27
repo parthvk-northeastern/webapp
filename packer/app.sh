17ce14b2bd32070b08c206871dfe271d0dcbbe0c
Parth Vasant Kataria <kataria.p@northeastern.edu>
2022-10-27 10:47:17 -0400

    test

--- packer/app.sh
+++ packer/app.sh
@@ -8,18 +8,19 @@ sudo apt-get install nginx -y
 sudo mkdir webapp
 # sudo mv routes config README.md models package-lock.json server.js packer test.js package.json ~/webapp/
 # sudo cp -r routes config README.md models package-lock.json server.js packer test.js package.json ~/webapp/
-# cd webapp && sudo curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh
+cd webapp && sudo curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh
 # cd webapp
 # sudo curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh
-# sudo bash nodesource_setup.sh
-# sudo apt-get install nodejs -y
-# sudo apt-get install npm -y
-# sudo apt install unzip
-# cd ~/ && unzip webapp.zip
-# cd ~/webapp && npm i
+sudo bash nodesource_setup.sh
+sudo apt-get install nodejs -y
+sudo apt-get install npm -y
+sudo apt install unzip
+cd..
+sudo unzip webapp.zip -d ~/webapp
+cd ~/webapp && sudo npm i
 
-# echo "Installing mysql server"
-# sudo apt-get install mysql-server -y
+echo "Installing mysql server"
+sudo apt-get install mysql-server -y
 # sudo mysql <<EOF
 # CREATE DATABASE nodemysql;
 # CREATE USER 'newuser'@'localhost' IDENTIFIED BY 'newpassword';
@@ -29,9 +30,9 @@ sudo mkdir webapp
 # echo "Starting mysql server"
 # sudo service mysql start
 
-# sudo npm i pm2
-# sudo npm i -g pm2
-# sudo pm2 start server.js
-# sudo pm2 startup systemd
+sudo npm i pm2
+sudo npm i -g pm2
+sudo pm2 start server.js
+sudo pm2 startup systemd
 
 sudo apt-get clean
